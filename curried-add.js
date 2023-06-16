2d72df95347ed73f3847c16a066e5100c5b1d77e
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-06-16 14:57:46 -0700

    adding curried function

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,11 @@
 function curriedAdd(total) {
+  if (!total) return 0;
 
+  return function adder(num) {
+    if (!num) return total;
+    total += num;
+    return adder;
+  };
 }
 
 module.exports = { curriedAdd };
