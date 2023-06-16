6cc82b9fae7a7239448f41933c187fb9a2b089a4
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-06-16 14:41:09 -0700

    adding create-account function

--- create-account.js
+++ create-account.js
@@ -1,5 +1,32 @@
-function createAccount(pin, amount) {
+const createAccount = (pin, amount = 0) => {
+  return {
+    checkBalance(inputPin) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      return `$${amount}`;
+    },
 
-}
+    deposit(inputPin, newAmount) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      amount += newAmount;
+      return `Succesfully deposited $${newAmount}. Current balance: $${amount}.`;
+    },
+
+    withdraw(inputPin, requiredAmount) {
+      if (inputPin !== pin) return "Invalid PIN.";
+
+      if (requiredAmount > amount)
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+
+      amount -= requiredAmount;
+      return `Succesfully withdrew $${requiredAmount}. Current balance: $${amount}.`;
+    },
+
+    changePin(inputPin, newPin) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      pin = newPin;
+      return "PIN successfully changed!";
+    },
+  };
+};
 
 module.exports = { createAccount };
