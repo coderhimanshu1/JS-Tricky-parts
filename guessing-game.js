5c05723f3b60c01572482a4054885a2bbe771a28
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-06-15 17:04:30 -0700

    adding guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,32 @@
 function guessingGame() {
+  //   generate a secret number
+  let secretNum = Math.floor(Math.random() * 100);
 
+  // Initialize guess count
+  guesses = 0;
+
+  //   set win status and initialize to false
+  hasWon = false;
+
+  return function guess(number) {
+    guesses++;
+
+    if (hasWon) {
+      return "The game is over, you already won!";
+    }
+
+    // if number = secretNum return secretNum and guesses
+    if (number === secretNum) {
+      hasWon = true;
+      return `You win! You found ${secretNum} in ${guesses} guesses.`;
+    }
+
+    // if number < secretNum return too low
+    if (number < secretNum) return `${number} is too low!`;
+
+    // if number > secretNum return too high
+    if (number > secretNum) return `${number} is too high!`;
+  };
 }
 
 module.exports = { guessingGame };
